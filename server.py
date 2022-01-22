cdf8664fcb78cee1a8ead3b605f03a0b72e7820c
Mauaqp <peraltilla93@hotmail.com>
2022-01-21 23:10:16 -0500

    Entrega final

--- server.py
+++ server.py
@@ -8,6 +8,15 @@ app.secret_key = "estoessecreto"
 def index():
     return render_template("index.html")
 
+#Ruta process data
+@app.route('/process', methods=['POST'])
+def post():
+    session["nombre"] = request.form["nombre"]
+    session["loc-dojo"] = request.form["loc-dojo"]
+    session["leng"] = request.form["leng"]
+    session["comments"] = request.form["comments"]
+    return render_template('result.html')
+
 #app.run
 if __name__=="__main__":
     app.run(debug=True)
